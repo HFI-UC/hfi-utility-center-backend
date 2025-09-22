1553982463e328f7b9e351d4e7ff80d1f374592d
SilianZ <silian_zheng@outlook.com>
2025-09-23 00:41:56 +0800

    fix: cookies

--- core/__init__.py
+++ core/__init__.py
@@ -432,7 +432,7 @@ async def admin_login(
     response = JSONResponse(
         BasicResponse(success=True, message="Login successful.").model_dump()
     )
-    response.set_cookie("UCCOOKIE", cookie, httponly=True)
+    response.set_cookie("UCCOOKIE", cookie, httponly=True, samesite='none')
     return response
 
 
