459d94f9b69ef19c68ef669b51da60596d3afc6e
SilianZ <silian_zheng@outlook.com>
2026-08-09 17:59:04 +0800

    chore: update cors

--- core/__init__.py
+++ core/__init__.py
@@ -42,7 +42,7 @@ app = FastAPI(lifespan=lifespan)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
+        "http://localhost:3000",
         "https://www.hfiuc.org",
         "https://preview.hfiuc.org",
     ],
